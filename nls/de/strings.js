d04ad9207fa566480a95c5a82e4dec69359f2ef7
Christian Knappke <info@k5e.de>
2014-11-26 11:45:45 +0100

    German translation - first commit

---
+++ nls/de/strings.js
@@ -0,0 +1,125 @@
+/**
+ * @preserve Copyright (c) 2014 ApptoIX. All rights reserved.
+ * @author Alexandre Bento Freire
+ * @translator Christian Knappke
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the 'Software'),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, white: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+  'HISTORY_HINT': ' Mit Strg+UP/DOWN durch die History blättern',
+  'SPACETEXT_HINT': ' \\$ für führende und nachfolgende Leerzeichen',
+  'SHORTCUT_HINT' : 'z.B.: Strg-Umsch-U (Win).  Cmd-Shift-U (Mac)',
+  'NEED_RESTART_MSG': 'Wird erst nach Neustart wirksam!',
+  'DEVBY_MSG': 'Entwickelt von {0}',
+  'EXTRACTOR_MSG' : 'Schreibe einn regulären Ausdruck<br>Alles was darauf passt wird in die Zwischenablage kopiert.<br>',  
+    
+  'FLD_limaxcharsperline_label': 'Max. Anzahl Zeichen pro Zeile',
+  'FLD_limaxcharsperline_hint': 'Auf 0 setzen, um den Zeilenumbruch zu verhindern',
+  'FLD_maxcharsperline_label': 'Max. Anzahl Zeichen pro Zeile',      
+  'FLD_maxcharsperline_hint': 'Mindestens 1',
+  'FLD_tobreakwords_label': 'Wörter umbrechen',
+  'FLD_tobreakwords_hint': 'Auf "true" setzen, damit Wörter umgebrochen werden können',
+  'FLD_lihtmlparawrap_label': 'In HTML Tag einhüllen',
+  'FLD_lihtmlparawrap_hint': 'Hüllt jeden Absatz in mit diesem Tag ein. Z.B.: p',
+  'FLD_linrparagraphs_label': 'Anzahl Absätze',
+  'FLD_linrparagraphs_hint': 'Maximal 100 Absätze',
+  'FLD_scss_label': 'scss Compiler',
+  'FLD_scss_hint': '"sass" muss zuerst installier werden. Siehe http://sass-lang.com/',
+  'FLD_scss_compass_label': 'Compass',
+  'FLD_scss_sass_label': 'SCSS',
+  'FLD_scss_autosave_hint': 'Automatisch sichern',
+  'FLD_js6_label': 'js6 Compiler',
+  'FLD_js6_hint': 'Zuerst nodejs installieren und dann "npm install -g traceur"',
+  'FLD_js6_autosave_hint': 'Automatisch sichern',
+  'FLD_splitMarker_label': 'Teilungsmarkierung',
+  'FLD_historySize_label': 'Größe der History',
+  'FLD_recentSize_label': 'Letzte Größe',
+  'FLD_startNum_label': 'Startwert',
+  'FLD_numSep_label': 'Separator nach Nummer',
+  'FLD_tabSize_label': 'Tab-Größe',
+  'FLD_find_label': 'Suche',
+  'FLD_replace_label': 'Ersetze',
+  'FLD_replace_hint': '$NUM$ als Macro zum Nummerieren',
+  'FLD_startValue_label': 'Start',
+  'FLD_startValue_hint': 'Leer lassen, um Nummerierung abzuschalten. Es verlangsamt den Prozess und wird auch nur von wenigen regulären Ausdrücken für die Textersetzung unterstützt. Reguläre Ausdrücke müssen aktiviert sein.',
+  'FLD_stepValue_label': 'Schrittweite',
+  'FLD_iswordsonly_label': 'Nur ganze Wörter',
+  'FLD_iswordsonly_hint': 'Funktioniert nur, wenn es kein regulärer Ausdruck ist',
+  'FLD_isregexpr_label': 'Regulärer Ausdruck',
+  'FLD_isignorecase_label': 'Groß-/kleinschreibung gnorieren',
+  'FLD_isimultiline_label': 'Mehrere Zeilen',
+  'FLD_isall_label': 'Alles ersetzen',  
+  'FLD_isselonly_label': 'Auf Auswahl beschränken',
+  'FLD_isselonly_hint': 'Wenn Text ausgewählt ist, nur dort ersetzen.',
+  'FLD_webSearch_label': 'URL der Suchmaschine',
+  'HDR_options_Field_label': 'Feld',
+  'HDR_options_Value_label': 'Wert',
+  'HDR_options_ExecOnSave_label': 'Beim Speichern ausführen',
+  
+  'UpperCase': 'Großbuchstaben',
+  'LowerCase': 'Kleinbuschstaben',
+  'Capitalize': 'Wortanfang groß',
+  'HtmlEncode': 'HTML codieren',
+  'HtmlDecode': 'HTML decodieren',
+  'UrlEncode': 'URL encodieren',
+  'Join': 'Zusammenfügen',
+  'Split': 'Teilen',
+  'Number': 'Nummer',
+  'Reverse': 'Umgekehrt',
+  'Trim Leading': 'Führenden Leerraum trimmen',
+  'Trim Trailing': 'Nachfolgenden Leerraum trimmen',
+  'Markdown Trim Trailing': 'Markdown: nachfolgenden Leerraum trimmen',
+  'Sort Ascending': 'Aufsteigend sortieren',
+  'Sort Descending': 'Absteigend sortieren',
+  'Remove Duplicates': 'Doppelte Zeilen entfernen',
+  'Remove Empty Lines': 'Leerzeilen entfernen',
+  'Unix To Win': 'Unix Pfadtrennen nach Windows',
+  'Win To Unix': 'Windows Pfadtrenner nach Unix',
+  'Single Slash To Double': 'Einfachen Schrägstrich verdoppeln',
+  'Double To Single Slash': 'Doppelten Schrägstrich zu einfachem',
+  'Single Quote To Double': 'Einfache Anführungszeichen zu doppelten',
+  'Double To Single Quote': 'Doppelte Anführungszeichen zu einfachen',
+  'Toggle Quote': 'Anführungszeichen umschalten',
+  'Tab To Space': 'Tabulatoren zu Leerzeichen',
+  'Space To Tab': 'Leerzeichen zu Tabulatoren',
+  'Untag': 'Tags entfernen',
+  'Tag': 'Tag',
+  'Break Line At': 'Zeile bei ... umbrechen',
+  'Function JSDoc': 'JSDoc Kommentar einfügen',
+  'Declare JSLint Global': 'Funktion als JSLint Global kennzeichnen',
+  'Extractor': 'Extraktor',
+  'Replace': 'Ersetzen',
+  'Open Url': 'URL öffnen',
+  'Web Search': 'Web-Suche',
+  'Browse File': 'Dateibrowser',
+  'Recent Files': 'Kürzlich geöffnete Dateien',
+  'Copy Filename': 'Dateiname kopieren',
+  'Copy Fullname': 'Vollstandigen Dateinamen kopieren',
+  'Html Report': 'HTML-Daten (id, class, style)',
+  'Regex Tester': 'RegEx-Tester',
+  'Compiler': 'Compile(js6, scss)',
+  'Commands': 'Kommandos',
+  'Commands Mapper': 'Kommandos zuordnen',
+  'Options': 'Optionen'
+});
